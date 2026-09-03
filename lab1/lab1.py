379338106edcd50658b5eb2bd7387b198862fd7b
luffi-07 <atulegenov508@gmail.com>
2026-09-03 16:16:51 +0500

    1

---
+++ lab1/lab1.py
@@ -0,0 +1,11 @@
+#a = 10
+#b = 4
+
+summa = 10 + 4
+raznost = 10 - 4
+proizvedenie = 10 * 4
+srednee = (10 + 4) / 2
+print("Сумма:", summa)
+print("Разность:", raznost)
+print("Произведение:", proizvedenie)
+print("Среднее арифметическое:", srednee)
